e24276cad6ae0613ea5aa3f6db9fcd919c3167e1
Robin Liu <robinliu106@gmail.com>
2020-06-24 21:10:01 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import {
+    StreetViewService,
+    StreetViewPanorama,
+    GoogleMap,
+    LoadScript,
+} from "@react-google-maps/api";
+
+const containerStyle = {
+    height: "400px",
+    width: "800px",
+};
+
+const center = {
+    lat: 51.5320665,
+    lng: -0.177203,
+};
+
+const App = () => {
+    return (
+        <LoadScript
+            googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                zoom={7}
+                center={center}
+            >
+                <StreetViewPanorama
+                    position={center}
+                    visible={true}
+                    options={{
+                        disableDefaultUI: true,
+                        enableCloseButton: false,
+                    }}
+                />
+            </GoogleMap>
+        </LoadScript>
+    );
+};
 
 export default App;
+
+/*
+ *           <MapBox
+                apiKey={"AIzaSyDZvOGdpcfvXcW7faf8eA4fG0JvKJ1dXDQ"}
+                opts={{
+                    center: { lat: 42.390053, lng: -71.142162 },
+                    zoom: 14,
+                }}
+                style={{
+                    height: "100vh",
+                    width: "100%",
+                }}
+                onCenterChanged={() => {
+                    console.log("The center of the map has changed.");
+                }}
+            />{" "} 
+ */
